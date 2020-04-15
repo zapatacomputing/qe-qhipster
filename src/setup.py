b9eea18dd119a1362d03c892dee9184eafb69592
michalstechly <michal.stechly@gmail.com>
2020-04-14 23:03:27 -0400

    Fixed depdencies and repo structure.

--- src/setup.py
+++ src/setup.py
@@ -10,17 +10,17 @@ setuptools.setup(
     version="0.1.0",
     author="Zapata Computing, Inc.",
     author_email="info@zapatacomputing.com",
-    description="qHiPSTER simulator for Orquestra.",
+    description="qHiPSTER simulator for Quantum Engine.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zapatacomputing/qe-qhipster ",
-    packages=setuptools.find_namespace_packages(include=['qe.*']),
+    packages=['qeqhipster'],
     package_dir={'' : 'python'},
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'zquantum-core'
+        'z-quantum-core'
     ]
 )
