a3d1881974207d5016ecf02b87bf53f95bf7820b
michalstechly <michal.stechly@gmail.com>
2020-05-26 16:28:12 -0400

    Added test for expectation values.

--- src/python/qeqhipster/simulator_test.py
+++ src/python/qeqhipster/simulator_test.py
@@ -1,6 +1,7 @@
 import unittest
 from zquantum.core.interfaces.backend_test import QuantumSimulatorTests
 from .simulator import QHipsterSimulator
+from pyquil.gates import X
 
 class TestQHipster(unittest.TestCase, QuantumSimulatorTests):
 
@@ -8,7 +9,19 @@ class TestQHipster(unittest.TestCase, QuantumSimulatorTests):
         self.wf_simulators = [QHipsterSimulator()]
         self.backends = [QHipsterSimulator(n_samples=1000), QHipsterSimulator()]
 
-
+    def test_get_expectation_values_for_Z(self):
+        # Given
+        circuit = Circuit(Program(X(0)))
+        operator = IsingOperator('3[Z0]')
+        target_expectation_values = np.array([-3])
+        n_samples = 1
+        # When
+        for backend in self.backends:
+            backend.n_samples = 1
+            expectation_values = backend.get_expectation_values(circuit, operator)
+            # Then
+            self.assertIsInstance(expectation_values, ExpectationValues)
+            np.testing.assert_array_almost_equal(expectation_values.values, target_expectation_values, decimal=15)
 
 
 
