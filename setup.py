f4354de73f958d64a92e16245884145c461a6b20
simonwa7 <simonwa7@gmail.com>
2020-09-30 13:22:44 -0400

    Adding setup.py file to root for v1

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import os
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="qe-qhipster",
+    version="0.1.0",
+    author="Zapata Computing, Inc.",
+    author_email="info@zapatacomputing.com",
+    description="qHiPSTER simulator for Quantum Engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zapatacomputing/qe-qhipster ",
+    packages=["qeqhipster"],
+    package_dir={"": "src/python"},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=["z-quantum-core"],
+)
