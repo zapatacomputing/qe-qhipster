f2994a957bf08e25d3e756670fdfd109f785ba86
michalstechly <michal.stechly@gmail.com>
2020-12-17 17:54:45 -0500

    Removed unnecessary code from tests.

--- src/python/qeqhipster/simulator_test.py
+++ src/python/qeqhipster/simulator_test.py
@@ -23,6 +23,4 @@ def wf_simulator(request):
 
 
 class TestQHipster(QuantumSimulatorTests):
-    def setUp(self):
-        self.wf_simulators = [QHipsterSimulator()]
-        self.backends = [QHipsterSimulator(n_samples=1000), QHipsterSimulator()]
+    pass
