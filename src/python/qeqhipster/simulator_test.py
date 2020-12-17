e71e6b30efd38de05f369a43f51e409675b5624d
michalstechly <michal.stechly@gmail.com>
2020-12-17 17:52:18 -0500

    Updated test to be up to date with new format.

--- src/python/qeqhipster/simulator_test.py
+++ src/python/qeqhipster/simulator_test.py
@@ -1,14 +1,28 @@
-import unittest
+import pytest
 from zquantum.core.interfaces.backend_test import QuantumSimulatorTests
 from .simulator import QHipsterSimulator
 
-class TestQHipster(unittest.TestCase, QuantumSimulatorTests):
 
+@pytest.fixture(
+    params=[
+        {},
+        {"n_samples": 1000},
+    ]
+)
+def backend(request):
+    return QHipsterSimulator(**request.param)
+
+
+@pytest.fixture(
+    params=[
+        {},
+    ]
+)
+def wf_simulator(request):
+    return QHipsterSimulator(**request.param)
+
+
+class TestQHipster(QuantumSimulatorTests):
     def setUp(self):
         self.wf_simulators = [QHipsterSimulator()]
         self.backends = [QHipsterSimulator(n_samples=1000), QHipsterSimulator()]
-
-
-
-
-
